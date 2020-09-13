rusty-snake/fjp

commit b348a9af614626b6039f91f784f46d9e0d9778e1

improve enable disable
 - better error msg
 - use new profile utils

src/enable.rs
@@ -19,8 +19,8 @@
 
 use crate::{
     disable::DISABLED_DIR,
-    profile_path,
-    utils::{get_name1, input},
+    profile::{NewProfileFlags, Profile},
+    utils::input,
     USER_PROFILE_DIR,
 };
 use clap::ArgMatches;
@@ -34,7 +34,13 @@ pub fn start(cli: &ArgMatches<'_>) {
         enable_user();
     } else {
         let profile_name = cli.value_of("PROFILE_NAME").unwrap();
-        enable_profile(&get_name1(profile_name));
+        enable_profile(
+            &Profile::new(
+                profile_name,
+                NewProfileFlags::LOOKUP_USER | NewProfileFlags::DENY_BY_PATH,
+            )
+            .unwrap(),
+        );
     }
 }
 
@@ -49,15 +55,21 @@ fn enable_user() {
         .unwrap_or_else(|err| error!("Rename failed: {}", err));
 }
 
-fn enable_profile(profile: &str) {
-    let disabled_profile = DISABLED_DIR.get_profile_path(profile);
+fn enable_profile(profile: &Profile) {
+    let disabled_profile = DISABLED_DIR.get_profile_path(profile.full_name());
     debug!("disabled profile: {}", disabled_profile.to_string_lossy());
 
-    let enabled_profile = profile_path!(USER / profile);
+    let enabled_profile;
+    if let Some(path) = profile.path() {
+        enabled_profile = path;
+    } else {
+        error!("{} is not disabled.", profile.full_name());
+        return;
+    }
     debug!("enabled profile: {}", enabled_profile.to_string_lossy());
 
     if enabled_profile.exists() {
-        warn!("Profile '{}' is alread enabled.", profile);
+        warn!("Profile '{}' is alread enabled.", profile.full_name());
         if input("Override? [Y/n] ").unwrap() != "y" {
             info!("Skipping");
             return;

src/disable.rs
@@ -19,8 +19,8 @@
 
 use crate::{
     location::Location,
-    profile_path,
-    utils::{get_name1, input},
+    profile::{NewProfileFlags, Profile},
+    utils::input,
     USER_PROFILE_DIR,
 };
 use clap::ArgMatches;
@@ -49,7 +49,13 @@ pub fn start(cli: &ArgMatches<'_>) {
         list().unwrap_or_else(|e| error!("An error occured while listing: {}", e));
     } else {
         let profile_name = cli.value_of("PROFILE_NAME").unwrap();
-        disable_profile(&get_name1(profile_name));
+        disable_profile(
+            &Profile::new(
+                profile_name,
+                NewProfileFlags::LOOKUP_USER | NewProfileFlags::DENY_BY_PATH,
+            )
+            .unwrap(),
+        );
     }
 }
 
@@ -72,20 +78,24 @@ fn list() -> IoResult<()> {
     Ok(())
 }
 
-fn disable_profile(profile: &str) {
-    let enabled_profile = profile_path!(USER / profile);
-    debug!("enabled profile: {}", enabled_profile.to_string_lossy());
-
-    if !enabled_profile.exists() {
-        error!("Profile '{}' does not exists.", profile);
+fn disable_profile(profile: &Profile) {
+    let enabled_profile;
+    if let Some(path) = profile.path() {
+        enabled_profile = path;
+    } else {
+        error!(
+            "Could not find '{}' in ~/.config/firejail",
+            profile.full_name()
+        );
         return;
     }
+    debug!("enabled profile: {}", enabled_profile.to_string_lossy());
 
-    let disabled_profile = DISABLED_DIR.get_profile_path(profile);
+    let disabled_profile = DISABLED_DIR.get_profile_path(profile.full_name());
     debug!("disabled profile: {}", disabled_profile.to_string_lossy());
 
     if disabled_profile.exists() {
-        warn!("Profile '{}' is alread disabled.", profile);
+        warn!("Profile '{}' is alread disabled.", profile.full_name());
         if input("Override? [Y/n] ").unwrap() != "y" {
             info!("Skipping");
             return;

commit e58d03e82c8bd166ecdb74fc1d3057af1f830280

fix enable.rs, was broken since b348a9a

src/enable.rs
@@ -37,7 +37,9 @@ pub fn start(cli: &ArgMatches<'_>) {
         enable_profile(
             &Profile::new(
                 profile_name,
-                ProfileFlags::LOOKUP_USER | ProfileFlags::DENY_BY_PATH,
+                ProfileFlags::LOOKUP_USER
+                    | ProfileFlags::ASSUME_EXISTENCE
+                    | ProfileFlags::DENY_BY_PATH,
             )
             .unwrap(),
         );
@@ -59,13 +61,13 @@ fn enable_profile(profile: &Profile) {
     let disabled_profile = DISABLED_DIR.get_profile_path(profile.full_name());
     debug!("disabled profile: {}", disabled_profile.to_string_lossy());
 
-    let enabled_profile;
-    if let Some(path) = profile.path() {
-        enabled_profile = path;
-    } else {
+    if !disabled_profile.exists() {
         error!("{} is not disabled.", profile.full_name());
         return;
     }
+
+    // NOTE: unwrap can't fail becuase profile is created with ASSUME_EXISTENCE.
+    let enabled_profile = profile.path().unwrap();
     debug!("enabled profile: {}", enabled_profile.to_string_lossy());
 
     if enabled_profile.exists() {